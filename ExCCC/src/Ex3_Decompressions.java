23a559ef877ac578eba4d9d289870df16bbe8f25
amandatishler <amandatishler@amandas-mbp.lan>
2021-01-08 23:35:22 -0500

    ccc

--- ExCCC/src/Ex3_Decompressions.java
+++ ExCCC/src/Ex3_Decompressions.java
@@ -5,17 +5,28 @@ public class Ex3_Decompressions {
 	public static void main(String[] args) {
 		
 		Scanner sc=new Scanner(System.in);
-		int L;
 		
-		L=sc.nextInt();
+		int L=sc.nextInt();
+		
+		int[] n = new int[L];
+		String[] s = new String[L];
 		
 		for (int i = 0; i < L; i++)
 		{
-			
+			n[i] = sc.nextInt();
+			s[i] = sc.next();
 		}
+			
+		for (int i = 0; i < L; i++)
+		{
+			for(int j = 0; j < n[i]; j++) 
+			{
+				System.out.print(s[i]);		
+			}
+			System.out.println();
+		}		
 		
-		
-	sc.close();
+		sc.close();
 	}
 
 	
--- ExCCC/src/Ex4_ParcStationnement.java
+++ ExCCC/src/Ex4_ParcStationnement.java
@@ -7,16 +7,16 @@ public class Ex4_ParcStationnement {
 		Scanner sc=new Scanner(System.in);
 		
 		int n = sc.nextInt();
-		String day1 = sc.nextLine();
-		String day2 = sc.nextLine();
-		int space = 0;
+		String day1 = sc.next();
+		String day2 = sc.next();
+		int occ = 0;
 		
 		for(int i = 0; i < n; i++)
 		{
 			if(day1.charAt(i) == 'C' && day2.charAt(i) == 'C')
-			space+=1;
+			occ+=1;
 		}
-		System.out.println(space);
+		System.out.println(occ);
 		
 		sc.close();
 	}
